0c3149356d6195e4dfe8c439ae9ff68e5d82cbc5
Matt Ryall <mryall@mawsonrovers.com>
2024-09-18 16:51:08 +1000

    Better output filenames, download both satellites

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use std::env;
+use chrono::{TimeDelta, Utc};
 use clap::{App, Arg};
+use parse_link_header::parse_with_rel;
 use reqwest::*;
 use serde::Deserialize;
-use std::fs::OpenOptions;
-use std::io::Write;
 use std::ops::Add;
-use std::path::Path;
-use chrono::{TimeDelta, Utc};
-use parse_link_header::parse_with_rel;
+use std::path::{Path, PathBuf};
+use std::{env, fs};
 
 #[derive(Debug, Deserialize)]
 struct DemodData {
@@ -67,9 +65,23 @@ struct Observation {
     transmitter_unconfirmed: Option<bool>,
 }
 
+impl Observation {
+    fn name(&self) -> String {
+        match self.start.as_ref() {
+            Some(start) => start.clone(),
+            None => self.id.to_string(),
+        }
+    }
+}
+
+struct Satellite {
+    name: &'static str,
+    id: &'static str,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let matches = App::new("satnogs_downloader")
+    let arguments = App::new("satnogs_downloader")
         .version("0.1.0")
         .author("Patrick Oppel")
         .about("Downloads satnogs observations")
@@ -85,78 +97,88 @@ async fn main() -> Result<()> {
             .value_name("END_DATE")
             .help("End date")
             .takes_value(true))
-        .arg(Arg::with_name("satellite_id")
-            .short('i')
-            .long("satellite_id")
-            .value_name("SATELLITE_ID")
-            .help("Satellite ID")
-            .takes_value(true))
         .get_matches();
 
-    let start_date = matches.value_of("start_date").unwrap_or("2024-08-16");
+    let sats = vec!(
+        Satellite { name: "CUAVA-2", id: "98858" },
+        Satellite { name: "WS-1", id: "98849" },
+    );
+
+    let start_date = arguments.value_of("start_date").unwrap_or("2024-08-16");
     let default_end_date = format!("{}", Utc::now().add(TimeDelta::days(1)).format("%F"));
-    let end_date = matches.value_of("end_date").unwrap_or(&default_end_date);
-    let satellite_id = matches.value_of("satellite_id").unwrap_or("98858");
+    let end_date = arguments.value_of("end_date").unwrap_or(&default_end_date);
     let api_token = env::var("SATNOGS_API_TOKEN").expect("Provide SATNOGS_API_TOKEN env var");
 
-    let mut url = format!("https://network.satnogs.org/api/observations/?start={}&end={}&satellite__norad_cat_id={}&status=good&format=json", start_date, end_date, satellite_id);
+    for sat in sats {
+        // create a new client for each satellite to avoid cursor problems on the server
+        let client = Client::new();
 
-    let client = Client::new();
+        let sat_folder = PathBuf::from(format!("download/{}", sat.name.to_lowercase()));
+        fs::create_dir_all(&sat_folder).expect("Unable to create folder");
 
-    loop {
-        println!("Querying API: {}", url);
-        let response = client.get(&url)
-            .header(header::AUTHORIZATION, format!("Bearer: {}", api_token))
-            .send()
-            .await?;
+        let params = format!("start={}&end={}&satellite__norad_cat_id={}&status=good&format=json",
+                             start_date, end_date, sat.id);
+        let mut url = format!("https://network.satnogs.org/api/observations/?{}", params);
 
-        let links = response.headers().get("Link");
-        let mut has_next = false;
-        if let Some(links) = links {
-            let links_str = links.to_str().unwrap();
-            println!("Found links: {}", links_str);
-            if let Ok(links) = parse_with_rel(links_str) {
-                if let Some(link) = links.get("next") {
-                    url = link.raw_uri.to_string();
-                    has_next = true;
-                }
+        loop {
+            println!("{}: Querying API: {}", sat.name, url);
+            let next = download_observations(&client, &sat, &sat_folder, &api_token, &url).await?;
+            match next {
+                Some(next) => url = next,
+                None => break,
             }
         }
+    }
+    Ok(())
+}
 
-        let observations: Vec<Observation> = serde_json::from_str(&response.text().await?).unwrap();
+async fn download_observations(client: &Client, sat: &Satellite, sat_folder: &PathBuf, api_token: &str, url: &str) -> Result<Option<String>> {
+    let response = client.get(url)
+        .header(header::AUTHORIZATION, format!("Bearer: {}", api_token))
+        .send()
+        .await?;
 
-        for observation in observations {
-            if observation.demoddata.is_empty() {
-                println!("Skipping observation with no data: {}", observation.id);
-            } else {
-                println!("Downloading observation: {}", observation.id);
-                let mut url = OpenOptions::new()
-                    .write(true)
-                    .create(true)
-                    .open(Path::new(&format!("./{}.url", observation.id))).expect("Unable to create file");
+    let next = find_next_url(&response);
 
-                let mut file = OpenOptions::new()
-                    .write(true)
-                    .create(true)
-                    .open(Path::new(&format!("./{}.raw", observation.id))).expect("Unable to create file");
+    let text = response.text().await?;
+    let observations: Vec<Observation> = serde_json::from_str(&text).unwrap();
 
-                for demod_data in observation.demoddata {
-                    let response = client.get(&demod_data.payload_demod).send().await?;
-                    file.write_all(response.bytes().await?.as_ref()).expect("Unable to write data");
-                    url.write_all(format!("{:?}\n", demod_data.payload_demod).as_bytes()).expect("Unable to write data");
-                }
-
-                // move files to folder
-                std::fs::create_dir_all(format!("./{}", satellite_id)).expect("Unable to create folder");
-                std::fs::rename(format!("./{}.url", observation.id), format!("./{}/{}.url", satellite_id, observation.id)).expect("Unable to move file");
-                std::fs::rename(format!("./{}.raw", observation.id), format!("./{}/{}.raw", satellite_id, observation.id)).expect("Unable to move file");
-            }
+    for obs in observations {
+        if obs.demoddata.is_empty() {
+            // Nothing to download!
+            continue;
         }
 
-        if !has_next {
-            break;
+        println!("{}: Downloading observation: {} ({})", sat.name, obs.name(), obs.id);
+        let url_file = output_file(&sat_folder, &sat, &obs, "url");
+        let data_file = output_file(&sat_folder, &sat, &obs, "raw");
+
+        for demod_data in obs.demoddata {
+            fs::write(&url_file, format!("{:?}\n", demod_data.payload_demod).as_bytes())
+                .expect("Unable to write data");
+
+            let response = client.get(&demod_data.payload_demod).send().await?;
+            fs::write(&data_file, response.bytes().await?.as_ref()).expect("Unable to write data");
         }
     }
 
-    Ok(())
+    Ok(next)
+}
+
+fn find_next_url(response: &Response) -> Option<String> {
+    let links = response.headers().get("Link")?;
+    match parse_with_rel(links.to_str().unwrap()) {
+        Ok(links) => {
+            let next_link = links.get("next")?;
+            Some(next_link.raw_uri.to_string())
+        },
+        Err(_) => None
+    }
+}
+
+fn output_file(parent: &Path, sat: &Satellite, obs: &Observation, suffix: &str) -> PathBuf {
+    let filename = format!("{}-{}-beacon.{}", obs.name(), sat.name.to_lowercase(), suffix);
+    let mut result = PathBuf::from(parent);
+    result.push(filename);
+    result
 }
